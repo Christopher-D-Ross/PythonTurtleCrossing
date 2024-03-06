c9259c9b55dd1f0be4c4fa9ba43f86f5b1ac774e
Christopher-D-Ross <ross758@gmail.com>
2024-03-06 16:58:29 -0500

    Adding layout files.

---
+++ player.py
@@ -0,0 +1,7 @@
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+# This class is for the turtle being controlled to cross the road.
+class Player:
+    pass
---
+++ scoreboard.py
@@ -0,0 +1,5 @@
+FONT = ("Courier", 24, "normal")
+
+# This class will show the current level and game over if hit by a car.
+class Scoreboard:
+    pass
